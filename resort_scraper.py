1c9edc619b273412ac27f8922eb40e60e87f1750
rupertn <rupertneve28@gmail.com>
2021-02-15 11:55:49 -0700

    added function comments

--- resort_scraper.py
+++ resort_scraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import pandas
 import time
+from datetime import datetime
 
 
 def get_resort_urls():
@@ -9,9 +11,11 @@ def get_resort_urls():
     region_list = ['britishcolumbia', 'alberta', 'montana', 'idaho', 'wyoming', 'utah', 'colorado', 'california',
                    'nevada', 'oregon', 'washington', 'arizona', 'newmexico', 'alaska']
 
+    region_l = ['arizona']
+
     resort_urls = []
 
-    for region in region_list:
+    for region in region_l:
         base_url = 'https://www.skicentral.com/'
         full_url = base_url + region + '.html'
 
@@ -28,6 +32,7 @@ def get_resort_urls():
 
 
 def get_resort_address(info, soup):
+    """Acquires the resort address, if available."""
     try:
         contact = soup.find('div', class_='addressblock')
         address = contact.text.split('Email:')[0].split('Address:')[1].strip()
@@ -38,19 +43,22 @@ def get_resort_address(info, soup):
 
 
 def get_overview_stats(info, soup):
-    overview_table = soup.find('table', id='mountainstatistics').find('tbody')  # main mountain stats
+    """Acquires the main resort statistics."""
+    overview_table = soup.find('table', id='mountainstatistics').find('tbody')
 
     for row in overview_table.find_all('tr')[:8]:
         info.append(row.find_all('td')[1].text)
 
 
 def get_lift_stats(info, soup):
-    lifts_table = soup.find('table', id='lifts').find('tbody')  # lift stats
+    """Finds the total number of lifts."""
+    lifts_table = soup.find('table', id='lifts').find('tbody')
     num_lifts = lifts_table.find('tr').find_all('td')[1].text
     info.append(num_lifts)
 
 
 def get_ticket_info(info, soup):
+    """Acquires the regular price for an adult single day ticket, if available."""
     try:
         tickets_table = soup.find('table', class_='tickettable table').find('tbody')
 
@@ -62,23 +70,24 @@ def get_ticket_info(info, soup):
                         price = int(col.text.split('$')[1])
                         if max_price is None or price > max_price:
                             max_price = price
-                    info.append(max_price)
+                    info.append(max_price)  # The max ticket price should equal the adult ticket price
 
     except AttributeError:
         info.append(None)
 
 
 def clean(info):
-    for idx, col in enumerate(info):
-        if (2 <= idx < 6) and (col != 'n/a' or col is not None):
-            info[idx] = col.split()[0]
+    for idx, value in enumerate(info):
+        if (2 <= idx < 6) and (value != 'n/a' or value is not None):
+            info[idx] = value.split()[0]
 
 
 def get_resort_info(mountain_url):
+    """Collects the resort information for a single resort."""
     page = requests.get(mountain_url)
     soup = bs(page.text, 'html.parser')
 
-    info = [soup.find('div', class_='resortname').text]  # resort name
+    info = [soup.find('div', class_='resortname').text]  # initialize list with resort name
 
     get_resort_address(info, soup)
     get_overview_stats(info, soup)
@@ -91,14 +100,16 @@ def get_resort_info(mountain_url):
 
 
 table = []
-start = time.time()
-
+start = datetime.now()
+print('Collecting resort URLs...')
 mountain_urls = get_resort_urls()
-print('Collected URLs for {} ski resorts.'.format(len(mountain_urls)))
+print('Collected URLs for {} resorts.'.format(len(mountain_urls)))
+print('Collecting resort information...')
 
 for url in mountain_urls:
     get_resort_info(url)
     time.sleep(1)
 
-print('Collected resort info for {} ski resorts'.format(len(table)))
-print('Completed scraping in: {}'.format(time.time() - start))
+print('Collected resort information for {} resorts.'.format(len(table)))
+print('Export to SQL Complete! The entire process took {}.'.format(datetime.now() - start))
+print(table)
